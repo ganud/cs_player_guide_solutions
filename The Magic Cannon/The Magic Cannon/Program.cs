0ed4984d5a8be702cf3ac96901e6b7e59b30f58a
devdundo <107737568+ganud@users.noreply.github.com>
2023-08-31 09:01:17 -0400

    The Magic Cannon solution

---
+++ The Magic Cannon/The Magic Cannon/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 &&  i % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Combined");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Electric");
+    }
+    else if (i % 3 == 0)
+    {
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Fire");
+    }
+    else {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Normal"); }
+}
