1bb4d6495359201b9276699d25e5cd2bf4086778
devdundo <107737568+ganud@users.noreply.github.com>
2023-08-31 09:25:51 -0400

    The Laws of Freach

---
+++ The Laws of Freach/The Laws of Freach/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int total = 0;
+foreach (int num in array)
+    total += num;
+float average = (float)total / array.Length;
+Console.WriteLine(average);
