e81272bc5c2aac6915099c76f4619276553a93ba
devdundo <107737568+ganud@users.noreply.github.com>
2023-09-01 09:09:26 -0400

    Countdown recursive solution

---
+++ Countdown/Countdown/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+void CountDown(int count)
+{
+    if (count == 0)
+    {
+        Console.WriteLine("0");
+        return;
+    }
+    Console.WriteLine(count);
+    count--;
+    CountDown(count);
+}
+
+CountDown(10);
