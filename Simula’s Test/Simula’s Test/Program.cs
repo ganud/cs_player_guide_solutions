04be6fbb04476c6f43f23569ffdcc40447b6765b
devdundo <107737568+ganud@users.noreply.github.com>
2023-09-03 09:31:45 -0400

    Simula’s Test

---
+++ Simula’s Test/Simula’s Test/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Chest box = Chest.locked;
+while (true) {
+    Console.WriteLine($"The chest is {box}. What would you like to do?");
+    string input = Console.ReadLine();
+    if (input == "open" && box == Chest.closed)
+    {
+        box = Chest.opened;
+    }
+    else if (input == "unlock" &&  box == Chest.locked) {
+        box = Chest.closed;
+    }
+    else if (input == "close" && box == Chest.opened)
+    {
+        box = Chest.closed;
+    }
+    else if (input == "lock" && box == Chest.closed)
+    {
+        box = Chest.locked;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Clear();
+        Console.WriteLine("Invalid: Can't transition between two states at once, or the state does not support that input");
+        Console.ForegroundColor= ConsoleColor.White;
+    }
+}
+enum Chest
+{
+    opened,
+    closed,
+    locked,
+}
+
