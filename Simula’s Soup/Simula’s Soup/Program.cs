fc093eba4827254fe46056c27be41b7a4a319105
devdundo <107737568+ganud@users.noreply.github.com>
2023-09-03 10:12:46 -0400

    Simula's Soup, 2/4 done

---
+++ Simula’s Soup/Simula’s Soup/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Souptype type = Souptype.soup;
+MainIngredients ingredient = MainIngredients.mushrooms;
+Seasonings seasoning = Seasonings.spicy;
+
+var Soup = (type, ingredient, seasoning);
+
+
+enum Souptype
+{
+    soup,
+    stew,
+    gumbo,
+}
+
+enum MainIngredients
+{
+    mushrooms,
+    chicken,
+    carrots,
+    potatoes,
+}
+
+enum Seasonings
+{
+    spicy,
+    salty,
+    sweet,
+}
