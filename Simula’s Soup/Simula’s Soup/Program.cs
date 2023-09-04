665e9fdbf3dbd6073264045b2e0bc82a80e368c3
devdundo <107737568+ganud@users.noreply.github.com>
2023-09-04 09:38:24 -0400

    Simula's soup solution

--- Simula’s Soup/Simula’s Soup/Program.cs
+++ Simula’s Soup/Simula’s Soup/Program.cs
@@ -1,30 +1,107 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Souptype type = Souptype.soup;
-MainIngredients ingredient = MainIngredients.mushrooms;
-Seasonings seasoning = Seasonings.spicy;
+﻿// Declarations
+Souptype type = Souptype.Soup;
+MainIngredients ingredient = MainIngredients.Mushrooms;
+Seasonings seasoning = Seasonings.Spicy;
 
+
+getSoupType();
+getMainIngredients();
+getSeasonings();
+// Tuple of the soup
 var Soup = (type, ingredient, seasoning);
+Console.WriteLine($"{seasoning} {ingredient} {type}");
+void getSoupType()
+{
+    Console.WriteLine("Select a soup type: soup, stew, or gumbo");
+    string input = Console.ReadLine();
+    Console.Clear();
+    switch (input)
+    {
+        case "soup":
+            type = Souptype.Soup;
+            break;
+        case "stew":
+            type = Souptype.Stew;
+            break;
+        case "gumbo":
+            type = Souptype.Gumbo;
+            break;
+        default:
+            Console.WriteLine("Invalid soup type");
+            getSoupType();
+            break;
+        
+    }
+}
 
+void getMainIngredients()
+{
+    Console.WriteLine("Select from the ingredients: mushrooms, chicken, carrots, or potatoes");
+    string input = Console.ReadLine();
+    Console.Clear();
+    switch (input)
+    {
+        case "mushrooms":
+            ingredient = MainIngredients.Mushrooms;
+            break;
+        case "chicken":
+            ingredient = MainIngredients.Chicken;
+            break;
+        case "carrots":
+            ingredient = MainIngredients.Carrots;
+            break;
+        case "potatoes":
+            ingredient = MainIngredients.Potatoes;
+            break;
+        default:
+            Console.WriteLine("Invalid ingredient");
+            getMainIngredients();
+            break;
 
+    }
+}
+
+void getSeasonings()
+{
+    Console.WriteLine("Select from the seasonings: spicy, salty, or sweet");
+    string input = Console.ReadLine();
+    Console.Clear();
+    switch (input)
+    {
+        case "spicy":
+            seasoning = Seasonings.Spicy;
+            break;
+        case "salty":
+            seasoning = Seasonings.Salty;
+            break;
+        case "sweet":
+            seasoning = Seasonings.Sweet;
+            break;
+        default:
+            Console.WriteLine("Invalid seasoning");
+            getSeasonings();
+            break;
+
+    }
+}
 enum Souptype
 {
-    soup,
-    stew,
-    gumbo,
+    Soup,
+    Stew,
+    Gumbo,
 }
 
 enum MainIngredients
 {
-    mushrooms,
-    chicken,
-    carrots,
-    potatoes,
+    Mushrooms,
+    Chicken,
+    Carrots,
+    Potatoes,
 }
 
 enum Seasonings
 {
-    spicy,
-    salty,
-    sweet,
+    Spicy,
+    Salty,
+    Sweet,
 }
