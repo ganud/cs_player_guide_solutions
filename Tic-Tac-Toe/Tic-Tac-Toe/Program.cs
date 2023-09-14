0b0aff64f85bd19f80e2c93472fbd1d70089faa8
devdundo <107737568+ganud@users.noreply.github.com>
2023-09-14 07:38:38 -0400

    TTT board print function completed

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+// The tic tac toe board is a 2D array. 0 is uncontested, 1 is player 1, 2 is player 2.
+
+using System.Data;
+
+TTTBoard board = new TTTBoard();
+board.printBoard();
+class TTTBoard
+{
+    private int[,] Board = { 
+        {0,0,0},
+        {0,0,0},
+        {0,0,0},
+    };
+    public TTTBoard()
+    {
+
+    }
+
+    public void printBoard()
+    {
+        // Code taken from a 2012 stack overflow answer with some edits https://stackoverflow.com/questions/12826760/printing-2d-array-in-matrix-format
+        Console.WriteLine("-------");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(string.Format("|{0}", Board[i, j]));
+            }
+            Console.Write("|\n-------\n");
+        }
+    }
+}
