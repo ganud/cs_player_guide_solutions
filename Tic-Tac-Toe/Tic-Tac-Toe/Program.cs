aa80dc6543c2836ed85cd1dd7f0762e8160c3d34
devdundo <107737568+ganud@users.noreply.github.com>
2023-09-15 07:31:07 -0400

    Add a player class

--- Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -2,15 +2,22 @@
 // The tic tac toe board is a 2D array. 0 is uncontested, 1 is player 1, 2 is player 2.
 
 using System.Data;
+using System.Diagnostics.Contracts;
+// While true
+// Prompt player 1's turn to the board with number commands, and replace with their symbol.
+// Prompt player 2 next
+// After each prompt check for a winner.
+Player player1 = new Player("Player 1", "O");
+Player player2 = new Player("Player 2", "X");
 
 TTTBoard board = new TTTBoard();
 board.printBoard();
 class TTTBoard
 {
-    private int[,] Board = { 
-        {0,0,0},
-        {0,0,0},
-        {0,0,0},
+    private string[,] Board = { 
+        {"0" ,"0" ,"0" },
+        {"0" ,"0" ,"0" },
+        {"0" ,"0" ,"0" },
     };
     public TTTBoard()
     {
@@ -31,3 +38,15 @@ class TTTBoard
         }
     }
 }
+
+class Player
+{
+    public string Name { get; private set; }
+    public string Symbol { get; private set; }
+    public Player(string name, string symbol)
+    {
+        Name = name;
+        Symbol = symbol;
+    }
+}
+
