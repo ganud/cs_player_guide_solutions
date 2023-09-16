041b1252abcb7f60d0c7ae835767fc8449eeb169
devdundo <107737568+ganud@users.noreply.github.com>
2023-09-16 10:05:26 -0400

    User input to board added

--- Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -7,17 +7,26 @@ using System.Diagnostics.Contracts;
 // Prompt player 1's turn to the board with number commands, and replace with their symbol.
 // Prompt player 2 next
 // After each prompt check for a winner.
+
 Player player1 = new Player("Player 1", "O");
 Player player2 = new Player("Player 2", "X");
 
 TTTBoard board = new TTTBoard();
 board.printBoard();
+
+while (true)
+{
+    board.userToBoard(player1.Name, player1.Symbol);
+    board.printBoard();
+}
+
+
 class TTTBoard
 {
     private string[,] Board = { 
-        {"0" ,"0" ,"0" },
-        {"0" ,"0" ,"0" },
-        {"0" ,"0" ,"0" },
+        {" " ," " ," " },
+        {" " ," " ," " },
+        {" " ," " ," " },
     };
     public TTTBoard()
     {
@@ -36,6 +45,92 @@ class TTTBoard
             }
             Console.Write("|\n-------------\n");
         }
+    }
+
+    public void userToBoard(string name, string symbol)
+    {
+        Console.WriteLine($"{name}, enter your tile (Use numpad as reference!)");
+        string input = Console.ReadLine();
+
+        // I'm not on board with code duplication, but I couldn't find a way for a function to work. 
+        // Fix code duplication later
+        switch (input)
+        {
+            case "1":
+                if (!Board[0, 0].Contains(" "))
+                {
+                    Console.WriteLine("That tile is taken.");
+                }
+                else { Board[0, 0] = symbol; }
+                break;
+            case "2":
+                if (!Board[0, 1].Contains(" "))
+                {
+                    Console.WriteLine("That tile is taken.");
+                }
+                else { Board[0, 1] = symbol; }
+                break;
+            case "3":
+                if (!Board[0, 2].Contains(" "))
+                {
+                    Console.WriteLine("That tile is taken.");
+                }
+                else { Board[0, 2] = symbol; }
+                break;
+            case "4":
+                if (!Board[1, 0].Contains(" "))
+                {
+                    Console.WriteLine("That tile is taken.");
+                }
+                else { Board[1, 0] = symbol; }
+                break;
+            case "5":
+                if (!Board[1, 1].Contains(" "))
+                {
+                    Console.WriteLine("That tile is taken.");
+                }
+                else { Board[1, 1] = symbol; }
+                break;
+            case "6":
+                if (!Board[1, 2].Contains(" "))
+                {
+                    Console.WriteLine("That tile is taken.");
+                }
+                else { Board[1, 2] = symbol; }
+                break;
+            case "7":
+                if (!Board[2, 0].Contains(" "))
+                {
+                    Console.WriteLine("That tile is taken.");
+                }
+                else { Board[2, 0] = symbol; }
+                break;
+            case "8":
+                if (!Board[2, 1].Contains(" "))
+                {
+                    Console.WriteLine("That tile is taken.");
+                }
+                else { Board[2, 1] = symbol; }
+                break;
+            case "9":
+                if (!Board[2, 2].Contains(" "))
+                {
+                    Console.WriteLine("That tile is taken.");
+                }
+                else { Board[2, 2] = symbol; }
+                break;
+        }
+    }
+
+    void inputTile(string coord, string symbol)
+    {
+        Console.WriteLine(Board[0,0]);
+        Console.WriteLine("wefwejweif");
+        if (!coord.Contains(" "))
+        {
+            Console.WriteLine("That tile is taken.");
+        }
+        else { coord = symbol; }
     }
 }
 
