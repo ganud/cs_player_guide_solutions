9f5a9289081f59d644016a37aa79e771c1a5783a
devdundo <107737568+ganud@users.noreply.github.com>
2023-09-17 10:09:40 -0400

    Remove pseudocode

--- Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -1,13 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-// The tic tac toe board is a 2D array. 0 is uncontested, 1 is player 1, 2 is player 2.
-
+﻿
 using System.Data;
 using System.Diagnostics.Contracts;
 using System.Security.Cryptography;
-// While true
-// Prompt player 1's turn to the board with number commands, and replace with their symbol.
-// Prompt player 2 next
-// After each prompt check for a winner.
 
 Player player1 = new Player("Player 1", "O");
 Player player2 = new Player("Player 2", "X");
@@ -17,6 +11,7 @@ board.printBoard();
 
 while (true)
 {
+    // Runs the game, check after every action
     board.userToBoard(player1.Name, player1.Symbol);
     Console.Clear();
     board.printBoard();
@@ -35,10 +30,6 @@ class TTTBoard
         {" " ," " ," " },
         {" " ," " ," " },
     };
-    public TTTBoard()
-    {
-
-    }
 
     public void printBoard()
     {
