5e6c237b85ee64eeb6e0b11d77871cdca4be6ace
devdundo <107737568+ganud@users.noreply.github.com>
2023-09-18 07:35:23 -0400

    Create classes

---
+++ Packing Inventory/Packing Inventory/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+
+
+
+class Inventory
+{
+    public float Weight { get; private set; }
+    public float Volume { get; private set; }
+    public Inventory()
+    {
+        Weight = 0;
+        Volume = 0;
+    }
+}
+
+class Arrow : Inventory
+{
+
+}
+
+class Bow : Inventory
+{
+
+}
+
+class Rope: Inventory
+{
+
+}
+
+class Water : Inventory
+{
+
+
+}
+
+class Rations : Inventory
+{
+
+
+}
+
+class Sword : Inventory
+{
+
+
+}
+
+class Pack
+{
+
+}
