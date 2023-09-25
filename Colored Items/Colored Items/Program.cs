ca4c5b9e334d2bb8009944ecbfce1d8f07f87168
devdundo <107737568+ganud@users.noreply.github.com>
2023-09-25 12:14:01 -0400

    Colored items solution

---
+++ Colored Items/Colored Items/Program.cs
@@ -0,0 +1,22 @@
+﻿
+ColoredItem<Sword> coloredsword = new ColoredItem<Sword>(ConsoleColor.Green, new Sword());
+coloredsword.Display();
+public class Sword { }
+public class Bow { }
+public class Axe { }
+
+public class ColoredItem<T>
+{
+    public ConsoleColor Color {  get;}
+    public T Item { get;}
+    public ColoredItem(ConsoleColor color, T item)
+    {
+        Color = color;
+        Item = item;
+    }
+    public void Display()
+    {
+        Console.ForegroundColor = Color;
+        Console.WriteLine(Item);
+    }
+}
