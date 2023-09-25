76321d75571d0ff5604838740686856b48dad4f5
devdundo <107737568+ganud@users.noreply.github.com>
2023-09-25 12:16:05 -0400

    Colored Items solution displaying 3 items

--- Colored Items/Colored Items/Program.cs
+++ Colored Items/Colored Items/Program.cs
@@ -1,6 +1,12 @@
 ﻿
 ColoredItem<Sword> coloredsword = new ColoredItem<Sword>(ConsoleColor.Green, new Sword());
+ColoredItem<Bow> coloredbow = new ColoredItem<Bow>(ConsoleColor.Blue, new Bow());
+ColoredItem<Axe> coloredaxe = new ColoredItem<Axe>(ConsoleColor.Red, new Axe());
+
 coloredsword.Display();
+coloredbow.Display();
+coloredaxe.Display();
+
 public class Sword { }
 public class Bow { }
 public class Axe { }
