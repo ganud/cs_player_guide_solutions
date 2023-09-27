279f1ec37555a63da4342dc3364ac3974e1b26de
devdundo <107737568+ganud@users.noreply.github.com>
2023-09-27 07:39:49 -0400

    Fountain pseudocode

---
+++ The Fountain of Objects/The Fountain of Objects/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Player has a row and column value, alterable by functions
+// Those values are used to index the board
+// If the string in the board has a specific value, do something.
+
+public class Map
+{
+    private string[,] Board = {
+        {"entrance" ," " ,"fountain", " " }, // Row
+        {" " ," " ," " , " "},
+        {" " ," " ," " , " " },
+        { " ", " ", " ", " " },
+    };
+    // Column
+    private bool FountainisOn = false;
+}
+
+public class Player : Map
+{
+    private int Row { get; set; } = 0;
+    private int Col { get; set; } = 0;
+}
