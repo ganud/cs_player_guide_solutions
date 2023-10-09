4992ad3cb5f9a5f3ae073e1f47f46adbde3f8e75
devdundo <107737568+ganud@users.noreply.github.com>
2023-10-09 10:49:06 -0400

    The Feud Solution

---
+++ The Feud/The Feud/Program.cs
@@ -0,0 +1,21 @@
+﻿using IField;
+using McDroid;
+using IFPig = IField.Pig;
+using MDPig = McDroid.Pig;
+
+MDPig McdroidPig = new MDPig();
+IFPig IFieldPig = new IFPig();
+Sheep sheep = new Sheep();
+Cow cow = new Cow();
+
+
+namespace IField
+{
+    class Pig { }
+    class Sheep { }
+}
+namespace McDroid
+{
+    class Pig { }
+    class Cow { }
+}
