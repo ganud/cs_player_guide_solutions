02347f912d8faba542a694bbd726fe73ee1f7818
devdundo <107737568+ganud@users.noreply.github.com>
2023-10-11 07:25:11 -0400

    Safer Number Crunching

---
+++ Safer Number Crunching/Safer Number Crunching/Program.cs
@@ -0,0 +1,26 @@
+﻿int integer;
+do
+{
+    Console.WriteLine("Enter an integer");
+}
+while (!int.TryParse(Console.ReadLine(), out integer));
+
+Console.WriteLine(integer);
+
+double duo;
+do
+{
+    Console.WriteLine("Enter an integer: output double");
+}
+while (!double.TryParse(Console.ReadLine(), out duo));
+
+Console.WriteLine(duo);
+
+bool truth;
+do
+{
+    Console.WriteLine("Enter true/false");
+}
+while (!bool.TryParse(Console.ReadLine(), out truth));
+
+Console.WriteLine(truth);
