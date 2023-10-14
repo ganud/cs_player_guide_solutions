31ba0313e7859f0323e5547c60a31cff95a0d757
devdundo <107737568+ganud@users.noreply.github.com>
2023-10-14 09:25:10 -0400

    Better Random Solution

---
+++ Better Random/Better Random/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Random rand = new Random();
+
+// Print 10 random floating point numbers 
+Console.WriteLine("Printing 10 random floating point numbers");
+
+public static class RandomExtension
+{
+    public static double NextDouble(int max = 1)
+    {
+        Random random = new Random();
+        return random.NextDouble() * max;
+    }
+    public static string RandString(params string[] strings)
+    {
+        Random rand = new Random();
+        int randIndex = rand.Next(0, strings.Length);
+        return strings[randIndex];
+    }
+
+    public static bool CoinFlip(double prob = 0.5)
+    {
+        Random rand = new Random();
+        return rand.NextDouble() > prob;
+    }
+}
