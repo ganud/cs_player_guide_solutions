85704dd13df4359012c8e23bf0bd723a92c00541
devdundo <107737568+ganud@users.noreply.github.com>
2023-10-14 09:41:27 -0400

    FIx misaligned sign in Coinflip

--- Better Random/Better Random/Program.cs
+++ Better Random/Better Random/Program.cs
@@ -1,5 +1,10 @@
-﻿Random rand = new Random();
-Console.WriteLine(rand.NextDouble(12));
+﻿using System.Collections.Generic;
+
+Random rand = new Random();
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(rand.CoinFlip(0.2));
+}
 public static class RandomExtension
 {
     public static double NextDouble(this Random random, int max = 1)
@@ -14,6 +19,6 @@ public static class RandomExtension
 
     public static bool CoinFlip(this Random random, double probHeads = 0.5)
     {
-        return random.NextDouble() > probHeads;
+        return random.NextDouble() < probHeads;
     }
 }
