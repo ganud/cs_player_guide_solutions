7871c12d0eb76c3a17709f766d6c702ef0db1b8e
devdundo <107737568+ganud@users.noreply.github.com>
2023-10-31 07:40:52 -0400

    Charberry tree partial solution

---
+++ Charberry Trees/Charberry Trees/Program.cs
@@ -0,0 +1,52 @@
+﻿CharberryTree tree = new CharberryTree();
+Notifier notifier = new Notifier(tree);
+while (true)
+{
+    tree.MaybeGrow();
+}
+
+public class CharberryTree
+{
+    private Random _random = new Random();
+
+    public event Action? Ripened;
+    public bool Ripe { get; set; }
+    public void MaybeGrow()
+    {
+        // Only a tiny chance of ripening each time, but we try a lot!
+        if (_random.NextDouble() < 0.00000001 && !Ripe)
+        {
+            Ripe = true;
+            Ripened();
+        }
+    }
+}
+
+public class Notifier
+{
+    private void OnTreeRipened() => NotifyRipened();
+    
+    public void NotifyRipened()
+    {
+        Console.WriteLine("The fruit is ripe!");
+    }
+    public Notifier(CharberryTree tree)
+    {
+        tree.Ripened += OnTreeRipened;
+    }
+}
+
+public class Harvester
+
+{
+    private void OnTreeRipened() => HarvestFruit();
+    public void HarvestFruit(CharberryTree tree)
+    {
+        Console.WriteLine("Fruit harvested.");
+        tree.Ripe = false;
+    }
+    public Harvester(CharberryTree tree)
+    {
+        tree.Ripened
+    }
+}
