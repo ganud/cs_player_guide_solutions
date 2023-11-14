c8d8cfd316d923f6a7ab8c4f04fe51e1f16c17b6
devdundo <107737568+ganud@users.noreply.github.com>
2023-11-14 06:59:03 -0500

    The Long Game Solution

---
+++ The Long Game/The Long Game/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter your name");
+string playerName = Console.ReadLine();
+int playerscore = 0;
+
+// Read previous score if name already exists
+if (File.Exists($"{playerName}.txt"))
+{
+    string previous = File.ReadAllText($"{playerName}.txt");
+    playerscore = int.Parse(previous);
+}
+
+
+while (true)
+{
+    if (Console.ReadKey().Key == ConsoleKey.Enter)
+    {
+        File.WriteAllText($"{playerName}.txt", Convert.ToString(playerscore));
+        break;
+    }
+    else
+    {
+        Console.WriteLine();
+        playerscore++;
+        Console.WriteLine($"Score: {playerscore}");
+    }
+}
