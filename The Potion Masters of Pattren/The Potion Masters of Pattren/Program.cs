941f1b6700843f69228fa59eda8ad9dd253c981b
devdundo <107737568+ganud@users.noreply.github.com>
2023-11-16 23:28:42 -0500

    Update readme

---
+++ The Potion Masters of Pattren/The Potion Masters of Pattren/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+
+
+
+enum PotionType { Water, Elixer, Poison, Flying, Invisibility, Night_Sight, Cloudy_Brew, Wraith}
+enum Ingredients { Stardust, Snake_Venom, Dragon_Breath, Shadow_Glass, Eyeshine_Gem}
